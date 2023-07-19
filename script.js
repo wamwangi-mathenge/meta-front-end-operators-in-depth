5448e604ffbc371130a0d00636338255b4842a44
Brian <wamwangimathenge@gmail.com>
2023-07-19 13:00:50 +0300

    Check if the currentTime is between 9am-5pm
    This commit comes up with code that will check if the currentTime
    variable is between 9am and 5pm.
    It first sets the currentTime variable and assigns it a value
    It also uses the && logical operator to join the 2 comparisons

---
+++ script.js
@@ -0,0 +1,11 @@
+// The Logical AND operator: &&
+
+// Used to confirm if multiple comparisons will return true.
+
+// Tasked with coming up with some code that will check if the currentTime variable is between 9am and 5pm.
+// The code needs to console.log true if currentTime > 9 and if currentTime < 17
+
+// Set the currentTime variable and assign the value of 10 to it.
+var currentTime = 10;
+// Use the && logical operator to join the two comparisons
+console.log(currentTime > 9 && currentTime < 17); // Returns True
